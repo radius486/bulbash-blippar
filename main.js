ce752b37d1a020e231b21f5c91e6d7e338e7ce79
Rodion Grishaev <radius486@gmail.com>
2016-12-10 02:31:35 +0300

    Objects and their positions.

--- main.js
+++ main.js
@@ -12,13 +12,16 @@ var mH = blipp.getMarker().getHeight();
 
 // Scene creation
 scene.onCreate = function() {
-    //var Plane = scene.addSprite().setColor('#ff7d32aa').setScale(mW, mH, 1);
-    //var speaker1 = scene.addTransform().read("hierarchy.json").setScale(mW, mH, 1);
-    //scene.setScale(mW, mH, 1);
+  //var Plane = scene.addSprite().setColor('#ff7d32aa').setScale(mW, mH, 1);
+  //var speaker1 = scene.addTransform().read("hierarchy.json").setScale(mW, mH, 1);
+  //scene.setScale(mW, mH, 1);
 
 	scene.speakerIn = scene.getChild("speaker_in");
 	scene.speakerOut = scene.getChild("speaker_out");
-    scene.speakerIn.setScale(5, 5, 5);
-    scene.speakerOut.setScale(5, 5, 5);
-    console.log(mW);
+  scene.year2017 = scene.getChild("year2017");
+    //scene.speakerIn.setScale(5, 5, 1);
+    //scene.speakerOut.setScale(5, 5, 1);
+    //scene.year2017.setTranslation(0, -1000, 0);
+    console.log('Height:'+ mH);
+    console.log('Width:' + mW);
 };
